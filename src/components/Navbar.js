da9633992534e060e110c35c8456c9d36caecb9f
Thales A. Paulino <thalesalves7839@gmail.com>
2022-07-14 21:38:08 -0300

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import IonIcon from "react-native-vector-icons/Ionicons";
+
+
+
+
+export default function Navbar(){
+    return(
+        <>
+            <IonIcon name="logo-instagram" />
+            <img src="./assets/img.logo.png" alt="logo"/>
+            
+            <h1>Instagram React</h1>
+        </>
+    )
+}
+
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import ReactDOM from 'reactdom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.querySelector(".root"));
